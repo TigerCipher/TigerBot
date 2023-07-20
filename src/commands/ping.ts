23fd50b49993e2bb6f2d8ec1258cc9a1f2ff8c55
Matthew Rogers <matt.rogers8396@gmail.com>
2023-07-19 23:59:22 -0500

    Added emojis to ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,24 +1,25 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { ChatInputCommandInteraction } from "discord.js";
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {ChatInputCommandInteraction} from "discord.js";
+import {emojis} from "../globals";
 
 const data = new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("Pong! Prints bot ping status information");
+    .setName("ping")
+    .setDescription("Pong! Prints bot ping status information");
 
 async function execute(interaction: ChatInputCommandInteraction) {
-  const sent = await interaction.reply({
-    content: "Pinging...",
-    fetchReply: true,
-  });
-  interaction.editReply(
-    `Pong!\nUptime: ${Math.round(
-      interaction.client.uptime / 60000
-    )} minutes\nWebsocket heartbeat: ${
-      interaction.client.ws.ping
-    } ms\nRoundtrip latency: ${
-      sent.createdTimestamp - interaction.createdTimestamp
-    } ms`
-  );
+    const sent = await interaction.reply({
+        content: "Pinging...",
+        fetchReply: true,
+    });
+    await interaction.editReply(
+        `${emojis["!"]}Pong!${emojis["!"]}\nUptime: ${Math.round(
+            interaction.client.uptime / 60000
+        )} minutes\nWebsocket heartbeat: ${
+            interaction.client.ws.ping
+        } ms\nRound-trip latency: ${
+            sent.createdTimestamp - interaction.createdTimestamp
+        } ms`
+    );
 }
 
-export { data, execute };
+export {data, execute};
